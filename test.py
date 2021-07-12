bdd337c0abf6357a37994817aa576fc26322917e
nomis51 <nomis51@users.noreply.github.com>
2021-07-11 20:23:26 -0400

    Add random tests script

---
+++ test.py
@@ -0,0 +1,4 @@
+from test_model import test_model_once
+
+test_model_once("./training/stack_size/stack_size.hdf5", "./images/tests/fusing6.png", (46, 46),
+                "./training/stack_size/stack_size_classes.txt", "6")
