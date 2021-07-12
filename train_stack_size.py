04313da221871ad5c5b02088f9b2e497ac1ad303
nomis51 <nomis51@users.noreply.github.com>
2021-07-12 09:40:18 -0400

    Add more images samples

--- train_stack_size.py
+++ train_stack_size.py
@@ -1,6 +1,6 @@
 from train import train_until_threshold, fix_crash
 
-size = (46,46)
+size = (46, 46)
 batch_size = 16
 epochs = 100
 training_dir = "./images/stack_sizes/training/"
@@ -12,4 +12,5 @@ step_epoch = 200
 expected_accuracy = 1.0
 
 fix_crash()
-model_file_path, classes_file_path = train_until_threshold(training_name, training_dir, size, test_dir, expected_accuracy, batch_size, epochs, step_epoch, new_training)
+train_until_threshold(training_name, training_dir, size, test_dir,
+                      expected_accuracy, batch_size, epochs, step_epoch, new_training)
